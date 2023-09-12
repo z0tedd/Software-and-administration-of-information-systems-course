c35c00264c11a7247bc1ea7ebf790c7e292cec34
z0tedd <z0tedd@gmail.com>
2023-09-12 14:40:05 +0000

    Updated some subjects

---
+++ basics_of_algorithms/Labs/1.2/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+const long double Pi = acos(-1.0);
+int main(){
+  //first task
+  /*
+  long double x, y, Result;
+	cin >> x >> y;
+	long double BigSquare = pow(x*pow(1+x,2)*pow(1+2*x,3))/(log(abs(tg(Pi/2-y)))),1/3);
+	Result = pow(x*pow(1+x,2)*pow(1+2*x,3)+ BigSquare, 1/5);
+	cout << result;
+	*/
+	//second task
+	/*
+	long double a,b,alfa,h, result;
+	cin >> a >> b >> alfa;
+	h = (a-b)/2 * tg(alfa * Pi / 180.0);
+	result = h*(a+b)/2
+	cout<< result;
+	*/
+	//Third task
+	int x,Result;
+	cin << x;
+	Result = x +
+
+	return 0;
+}
