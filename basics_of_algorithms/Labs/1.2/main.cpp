02426c3c33ef37da04a07854b117686863fe694d
z0tedd <z0tedd@gmail.com>
2023-09-24 12:45:07 +0000

    Update of the week

--- basics_of_algorithms/Labs/1.2/main.cpp
+++ basics_of_algorithms/Labs/1.2/main.cpp
@@ -8,22 +8,24 @@ int main(){
   /*
   long double x, y, Result;
 	cin >> x >> y;
-	long double BigSquare = pow(x*pow(1+x,2)*pow(1+2*x,3))/(log(abs(tg(Pi/2-y)))),1/3);
-	Result = pow(x*pow(1+x,2)*pow(1+2*x,3)+ BigSquare, 1/5);
-	cout << result;
+	long double BigSquare = pow((x*pow(1+x,2)*pow(1+2*x,3))/(log(abs(tan(Pi/2-y)))),1.0/3);
+	Result = pow(x*pow(1+x,2)*pow(1+2*x,3)+ BigSquare, 1.0/5);
+	cout << Result;
 	*/
 	//second task
-	/*
+	///*
 	long double a,b,alfa,h, result;
 	cin >> a >> b >> alfa;
-	h = (a-b)/2 * tg(alfa * Pi / 180.0);
-	result = h*(a+b)/2
+	h = (abs(a-b))/2 * tan(alfa * Pi / 180.0);
+	result =floor(h*(a+b)/2);
 	cout<< result;
-	*/
+	//*/
 	//Third task
+	/*
 	int x,Result;
 	cin >> x;
 	Result = (x/100)*100 + (x/10)%10 + (x%10)*10;
 	cout<< Result<< x%10;
+	*/
 	return 0;
 }
