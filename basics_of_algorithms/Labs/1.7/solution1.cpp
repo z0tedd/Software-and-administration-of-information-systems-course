587498d9eabfd353121f3fd5c9cf4966463d406b
z0tedd <z0tedd@gmail.com>
2023-12-04 15:32:16 +0000

    Update

--- basics_of_algorithms/Labs/1.7/solution1.cpp
+++ basics_of_algorithms/Labs/1.7/solution1.cpp
@@ -22,9 +22,21 @@ int sum_of_numbers(int x){
 	return s;
 }
 
-int Going_througth_square(vector<vector<int>> mas,int n){
-			
-return m;
+int Going_througth_square(vector<vector<int>> mas,int n,int k){
+	int s=0;
+	int final2 = 0;
+	int step2 = n/2;
+	for (int i = 0;i<n;i++){
+			for (int x =0;x<=final2;x++){
+					if (is_prime(mas[i][step2 + x]) and (sum_of_numbers(mas[i][step2+x ])%k)){
+						s+=mas[i][step2 + x];
+					}
+			}
+			final2++;
+
+	}
+
+return s;
 }
 
 
@@ -32,12 +44,12 @@ int main(){
 	ios::sync_with_stdio(0);
 	cout.tie(0);
 	cin.tie(0);
-	const n = 13;
-	vector<vector<int>> mas(n,vector<int>(16,0))
+	const int n = 13;
+	vector<vector<int>> mas(n,vector<int>(16,0));
 	cout << "Введите по строчкам массив 13*13"<<"\n";
 	for (int i=0;i<n;i++){
 		for (int j = 0; j<n;j++){
-				cin >> mas
+				cin >> mas[i][j];
 		}
 	}
 	/*
@@ -47,7 +59,7 @@ int main(){
 		cout << sum_of_numbers(test)<< " ";
 		cout << is_prime(test) << "\n";
 	}
-	
+
   cout << sum_of_numbers(444);
 	cout << sum_of_numbers(1000);
 */
