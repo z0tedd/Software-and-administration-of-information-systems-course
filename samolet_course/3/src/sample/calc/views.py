88527569c0e57830a49895a588e4b801c255fc03
z0tedd <z0tedd@gmail.com>
2024-05-15 19:04:44 +0000

    chk

--- samolet_course/3/src/sample/calc/views.py
+++ samolet_course/3/src/sample/calc/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def calculate(request):
+    expression = request.Get.get("expression")
+    try:
+        #result = (expression)
+    except Exception as e:
+        return HttpResponse(e, status=400)
+    return HttpResponse(result)
+
 
 # Create your views here.
